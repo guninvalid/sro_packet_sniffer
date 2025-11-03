73f7afc280d608ea5390a96719cb551697a96a4d
guninvalid <allenzfliu@gmail.com>
2025-11-02 23:41:37 -0800

    git please kindly work

--- sniffer.py
+++ sniffer.py
@@ -19,9 +19,9 @@ class Packet:
     self.packet = ppacket
     if (self.packet.haslayer(TCP)):
       self.tcp = self.packet[IP][TCP]
-    src_ip = self.packet[IP].src
-    dst_ip = self.packet[IP].dst
-    if src_ip == TARGET_IP or dst_ip == TARGET_IP:
+    self.src_ip = self.packet[IP].src
+    self.dst_ip = self.packet[IP].dst
+    if self.src_ip == TARGET_IP or self.dst_ip == TARGET_IP:
       self.set_flags()
       self.parse_data()
   def set_flags(self):
@@ -45,19 +45,40 @@ class Packet:
   def parse_data(self):
     if (not self.PSH_FLAG):
       return #if no data no bother
-    data_bytes = self.tcp.payload.load
-    packet_preamble = data_bytes[0:2].hex() # initial length
-    data_bytes = data_bytes[2:]
-    packet_addendum = data_bytes.hex()
-    if (data_bytes[0] == 0 and data_bytes[1] == 107):
-      num_bytes = (data_bytes[2] * 256) + data_bytes[3]
+    raw_data_bytes = self.tcp.payload.load
+    self.data_length = parse_bytes_to_num(raw_data_bytes[0:2]) # initial length
+    self.data_bytes = raw_data_bytes[2:]
+    self.op_code = parse_bytes_to_num(self.data_bytes[0:2])
+    if (self.op_code == 107):
+      num_bytes = (self.data_bytes[2] * 256) + self.data_bytes[3]
       print(num_bytes)
-      code = 0;
+      code = 0
       OFFSET = 4; #const
       for i in range(0, num_bytes):
-        code += (10 ** (num_bytes - i - 1)) * (data_bytes[OFFSET + i] - 48)
-      raw = ENCRYPTION_NUM_LOOKUP_ARRAY[code]
-      packet_string += f"[{packet_preamble}] {packet_addendum}"
+        code += (10 ** (num_bytes - i - 1)) * (self.data_bytes[OFFSET + i] - 48)
+      Packet.encryption_num = code;
+      Packet.encryption_raw = ENCRYPTION_NUM_LOOKUP_ARRAY[code]
+      self.packet_addendum = f"Encryption key num passed! Caught num {code}."
+      self.packet_addendum += f" (Raw: {Packet.encryption_raw})"
+    else:
+      # attempt to decrypt
+      self.decrypt(Packet.encryption_raw)
+  def decrypt(self, encryption_key):
+    if (encryption_key == ""): return
+    final_data = []
+    for i in range(0, len(self.data_bytes)):
+      byte = self.data_bytes[i];
+      offset = encryption_key[(i % len(encryption_key)):(i % len(encryption_key))].encode('utf-8')[0]
+      final_byte = byte - offset
+      while final_byte < -128:
+        final_byte += 256
+      final_data[i] = final_byte
+    self.decrypted_data = final_data
+  def print(self):
+    if (self.decrypted_data):
+      self.packet_addendum = self.decrypted_data.hex()
+    return f"Packet: {self.src_ip} -> {self.dst_ip}: {packet_addendum}"
+      
 def main():
   print(f"Starting packet capture for {TARGET_IP}...")
   # ENCRYPTION_NUM_LOOKUP_ARRAY = gen_lookup_array(121243)
@@ -72,26 +93,13 @@ def parse_bytes_to_num(byte_array):
 
 def packet_handler(dum_packet):
   packet = Packet(dum_packet)
+  print(packet.print())
 
 def encryption_code_lookup(num):
   key_raw = ENCRYPTION_NUM_LOOKUP_ARRAY[num]
   # key_sha = str(sha256(key_raw))[0:15]
   # return (key_raw, key_sha)
   return (key_raw, None)
-
-def decrypt(key, data):
-  if (key == ""): return
-
-  final_data = []
-
-  for i in range(0, len(data)):
-    byte = data[i];
-    offset = key[(i % len(key)):(i % len(key))].encode('utf-8')[0]
-    final_byte = byte - offset
-    while final_byte < -128:
-      final_byte += 256
-    final_data[i] = final_byte
-  return final_data
 
 
 main()
