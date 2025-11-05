ce5c9ae0eac06b67dd541980aecdc29b3e42b4c2
guninvalid <allenzfliu@gmail.com>
2025-11-05 01:05:35 -0800

    summary changes, now attempts to solve wrong length messages

--- Packet.py
+++ Packet.py
@@ -1,7 +1,9 @@
 from scapy.all import sniff
 from scapy.all import IP,TCP
+from scapy.all import Packet as SCPacket
 from hashlib import sha256
-from config import DEBUG,TARGET_IP
+from config import TARGET_IP
+from logger import debug,info,warning,error,fatal,warn
 
 def gen_lookup_array(N):
   # if it doesnt exist then generate the array
@@ -16,17 +18,17 @@ ENCRYPTION_NUM_LOOKUP_ARRAY = gen_lookup_array(121243)
 
 class Packet:
   
-  def __init__(self, ppacket):
+  def __init__(self, ppacket:SCPacket):
     # initializers
-    self.packet = ""
-    self.tcp = ""
-    self.src_ip = ""
-    self.dst_ip = ""
-    self.FIN_FLAG = ""; self.SYN_FLAG = ""; self.RST_FLAG = ""; self.PSH_FLAG = ""; self.ACK_FLAG = ""; self.URG_FLAG = ""; self.ECE_FLAG = ""; self.CWR_FLAG = "";
-    self.data_length = 0; self.data_bytes = "";
-    self.op_code = ""
-    self.packet_addendum = ""
-    self.decrypted_data = ""
+    self.packet:SCPacket
+    self.tcp:SCPacket
+    self.src_ip:str
+    self.dst_ip:str
+    self.FIN_FLAG:bool; self.SYN_FLAG:bool; self.RST_FLAG:bool; self.PSH_FLAG:bool; self.ACK_FLAG:bool; self.URG_FLAG:bool; self.ECE_FLAG:bool; self.CWR_FLAG:bool;
+    self.data_length:int = -1; self.data_bytes:bytes;
+    self.op_code:int
+    self.packet_addendum:str
+    self.decrypted_data:bytes
     
 
     self.packet = ppacket
@@ -38,7 +40,7 @@ class Packet:
     if self.src_ip == TARGET_IP or self.dst_ip == TARGET_IP:
       self.set_flags()
       self.parse_data()
-  def set_flags(self):
+  def set_flags(self) -> None:
     flag_depreciator = self.tcp.flags.value;
     self.FIN_FLAG = flag_depreciator % 2 == 1;
     flag_depreciator = flag_depreciator >> 1;
@@ -57,50 +59,79 @@ class Packet:
     self.CWR_FLAG = flag_depreciator % 2 == 1;
     flag_depreciator = flag_depreciator >> 1;
     self.packet_addendum = self.tcp.flags
-  def parse_data(self):
+  def parse_data(self) -> None:
     if (self.PSH_FLAG == False):
+      self.data_length = 0
       return #if no data no bother
-    raw_data_bytes = self.tcp.payload.load
+    raw_data_bytes:bytes = self.tcp.payload.load
     self.data_length = parse_bytes_to_num(raw_data_bytes[0:2]) # initial length
     self.data_bytes = raw_data_bytes[2:]
+    if (self.data_length != len(self.data_bytes)):
+      # allegedly in some cases the length will be in ascii not hex
+      if (30 <= raw_data_bytes[0] and raw_data_bytes[0] <= 39
+          and 30 <= raw_data_bytes[1] and raw_data_bytes[1] <= 39):
+        # attempt to process as ascii length
+        ascii_int:int = raw_data_bytes[1] - 30
+        ascii_int += 10 * (raw_data_bytes[0] - 30)
+        self.data_length = ascii_int
+        # test again
+        if (self.data_length != len(self.data_bytes)):
+          warn("Malformed packet! Lengths in hex and ascii do not match!")
+          warn("Attempting to auto assign to len(data_length).")
+          self.data_length = len(self.data_bytes)
+      else:
+        warn("Malformed packet! Lengths in hex do not match!")
+    if (self.data_length != len(self.data_bytes)):
+        warn("Attempting to auto assign to len(data_length).")
+        warn(f"Existing length: {self.data_length}")
+        self.data_length = len(self.data_bytes)
+        warn(f"New length: {self.data_length}")
     self.op_code = parse_bytes_to_num(self.data_bytes[0:2])
     self.packet_addendum = f"[RAW] {self.data_bytes.hex()}"
     if (self.op_code == 107):
       num_bytes = (self.data_bytes[2] * 256) + self.data_bytes[3]
     #   print(num_bytes)
-      code = 0
-      OFFSET = 4; #const
+      code:int = 0
+      OFFSET:int = 4; #const
       for i in range(0, num_bytes):
         code += (10 ** (num_bytes - i - 1)) * (self.data_bytes[OFFSET + i] - 48)
-      Packet.encryption_num = code;
-      Packet.encryption_raw = ENCRYPTION_NUM_LOOKUP_ARRAY[code]
+      Packet.encryption_num:int = code;
+      Packet.encryption_raw:int = ENCRYPTION_NUM_LOOKUP_ARRAY[code]
       sha_encoder = sha256()
       sha_encoder.update(str(Packet.encryption_raw).encode('ascii'))
-      Packet.encryption_key = sha_encoder.hexdigest()[0:16] # i am surprised this works
-      self.packet_addendum = f"Encryption key num passed! Caught num {code}."
-      self.packet_addendum += f" (Raw: {Packet.encryption_raw}, key: {Packet.encryption_key})"
+      Packet.encryption_key:str = sha_encoder.hexdigest()[0:16] # i am surprised this works
+      encryption_key_message:str = f"Caught num {code}."
+      encryption_key_message += f" (Raw: {Packet.encryption_raw}, key: {Packet.encryption_key})"
+      self.packet_addendum = f"Encryption key num passed! "
+      self.packet_addendum += encryption_key_message
+      # warn(encryption_key_message)
     else:
       # attempt to decrypt
       self.decrypt(Packet.encryption_key)
-  def decrypt(self, encryption_key):
+  def decrypt(self, encryption_key) -> None:
     if (encryption_key == ""): return
-    debug("Decrypyting!")
-    final_data: list[bytes] = [b"0"] * self.data_length
-    for i in range(0, len(self.data_bytes)):
-      byte = self.data_bytes[i];
-      key_length = len(encryption_key)
-      offset = encryption_key[(i % key_length)].encode('utf-8')[0]
-      final_byte = byte - offset
-      while final_byte < 0:
-        final_byte += 256
-      final_data[i] = final_byte
+    final_data:bytes = decrypt(self.data_bytes, encryption_key)
     self.decrypted_data : bytes = bytes(final_data)
     self.packet_addendum = f"[DEC]: {self.decrypted_data.hex()}"
-  def print(self):
+  def print(self) -> str:
     # ok so what i want is
     # if there 
     # nvm
     return f"Packet {self.src_ip} -> {self.dst_ip}: [{self.data_length}B] {self.packet_addendum}"
+
+def decrypt(data_bytes:bytes, encryption_key:str) -> bytes:
+  debug("Decrypyting!")
+  data_length:int = len(data_bytes)
+  final_data:list[bytes] = [b'0'] * data_length
+  key_length:int = len(encryption_key)
+  for i in range(0, data_length):
+    byte = data_bytes[i];
+    offset = encryption_key[(i % key_length)].encode('utf-8')[0]
+    final_byte = byte - offset
+    while final_byte < 0:
+      final_byte += 256
+    final_data[i] = int.to_bytes(final_byte)
+  return b''.join(final_data)
 
 def parse_bytes_to_num(byte_array):
   accumulator = 0
@@ -109,12 +140,12 @@ def parse_bytes_to_num(byte_array):
     accumulator += (256 ** (N - i - 1)) * byte_array[i]
   return accumulator
 
-def encryption_code_lookup(num):
-  key_raw = ENCRYPTION_NUM_LOOKUP_ARRAY[num]
-  # key_sha = str(sha256(key_raw))[0:15]
-  # return (key_raw, key_sha)
-  return (key_raw, None)
+# def encryption_code_lookup(num):
+#   key_raw = ENCRYPTION_NUM_LOOKUP_ARRAY[num]
+#   # key_sha = str(sha256(key_raw))[0:15]
+#   # return (key_raw, key_sha)
+#   return key_raw
 
-Packet.encryption_raw = ""
-Packet.encryption_num = ""
+Packet.encryption_raw = -1
+Packet.encryption_num = -1
 Packet.encryption_key = ""
--- sniffer.py
+++ sniffer.py
@@ -1,5 +1,7 @@
 from scapy.all import sniff
 from scapy.all import IP,TCP
+from scapy.all import Packet as SCPacket
+from scapy.all import PacketList
 from hashlib import sha256
 from Packet import Packet
 from config import TARGET_IP,PACKET_COUNT
@@ -8,16 +10,17 @@ from logger import debug, info, warn, error, fatal
 def main():
   print(f"Starting packet capture for {TARGET_IP}...")
   # ENCRYPTION_NUM_LOOKUP_ARRAY = gen_lookup_array(121243)
-  packets = sniff(filter=f"host {TARGET_IP}", prn=packet_handler, count=PACKET_COUNT)
+  packets:PacketList = sniff(filter=f"host {TARGET_IP}", prn=packet_handler, count=PACKET_COUNT)
 
-def packet_handler(dum_packet):
+def packet_handler(dum_packet:SCPacket):
   try:
-    packet = Packet(dum_packet)
+    packet:Packet = Packet(dum_packet)
     info(packet.print())
   except Exception as e:
     error("There was an error!")
-    error("Erorr: " + str(e))
-    error("Offending packet: " + dum_packet.summary() + dum_packet[IP][TCP].load.hex())
+    error("Error: " + str(e))
+    error("Offending packet: " + dum_packet.summary())
+    error("Load hex: " + dum_packet[IP][TCP].load.hex())
 
 
 main()
