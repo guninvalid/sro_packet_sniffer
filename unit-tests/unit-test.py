3d97c997ee9bccc6e0de6b63f04a78db803baa95
guninvalid <guninvalid@gmail.com>
2025-11-12 20:43:40 -0800

    another test commit

--- unit-tests/unit-test.py
+++ unit-tests/unit-test.py
@@ -1,6 +1,7 @@
 from scapy.all import Packet
 from logger import info
 
+info("Attempting packet processing...")
 packet = sniff(offline="sample_packets/capture.pcap", count=1)
 assert (packet != None)
 info("Assertion passed!")
