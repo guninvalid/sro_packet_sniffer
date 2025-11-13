5bd3a5bcdf6e4a7bd7a84bfd96c568c872a5826f
guninvalid <guninvalid@gmail.com>
2025-11-12 20:51:14 -0800

    more tests

--- unit-tests/unit-test.py
+++ unit-tests/unit-test.py
@@ -5,3 +5,8 @@ info("Attempting packet processing...")
 packet = sniff(offline="sample_packets/capture.pcap", count=1)
 assert (packet != None)
 info("Assertion passed!")
+
+info("Testing flags...")
+assert (packet.SYN_FLAG == True)
+assert (packet.FIN_FLAG == False)
+info("Assertion passed!")
